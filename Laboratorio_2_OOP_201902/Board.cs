e98753edcd0c3547d1d47c612f19059d96215de1
Juan Guillermo <jvergara5@miuandes.cl>
2019-08-23 10:21:20 -0400

    metodos getter

--- Laboratorio_2_OOP_201902/Board.cs
+++ Laboratorio_2_OOP_201902/Board.cs
@@ -145,45 +145,20 @@ namespace Laboratorio_2_OOP_201902
 
         }
     
-        public int[] GetMeleeAttackPoints()
+        public int[] GetAttackPoints()
         {
-            //Debe sumar todos los puntos de ataque de las cartas melee y retornar los valores por jugador.
             int[] totalAttack = new int[] { 0, 0 };
             for (int i=0; i < 2; i++)
             {
-                foreach (CombatCard meleeCard in meleeCards[i])
+                foreach (Card card in PlayerCards[i]["CombatCards"])
                 {
-                    totalAttack[i] += meleeCard.AttackPoints;
+                    totalAttack[i] += card.AttackPoints;
                 }
             }
             return totalAttack;
             
         }
-        public int[] GetRangeAttackPoints()
-        {
-            //Debe sumar todos los puntos de ataque de las cartas range y retornar los valores por jugador.
-            int[] totalAttack = new int[] { 0, 0 };
-            for (int i = 0; i < 2; i++)
-            {
-                foreach (CombatCard rangeCard in rangeCards[i])
-                {
-                    totalAttack[i] += rangeCard.AttackPoints;
-                }
-            }
-            return totalAttack;
-        }
-        public int[] GetLongRangeAttackPoints()
-        {
-            //Debe sumar todos los puntos de ataque de las cartas longRange y retornar los valores por jugador.
-            int[] totalAttack = new int[] { 0, 0 };
-            for (int i = 0; i < 2; i++)
-            {
-                foreach (CombatCard longRangeCard in longRangeCards[i])
-                {
-                    totalAttack[i] += longRangeCard.AttackPoints;
-                }
-            }
-            return totalAttack;
-        }
+       
+        
     }
 }
