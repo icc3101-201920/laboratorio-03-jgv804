8bbf97d7da1cdaad10f3500843228402f31950ae
Juan Guillermo <jvergara5@miuandes.cl>
2019-08-23 08:51:01 -0400

    Polimorfismo Deck

--- Laboratorio_2_OOP_201902/Deck.cs
+++ Laboratorio_2_OOP_201902/Deck.cs
@@ -7,21 +7,21 @@ namespace Laboratorio_2_OOP_201902
 {
     public class Deck
     {
-        private List<CombatCard> combatCards;
-        private List<SpecialCard> SpecialCards;
+        private List<Card> DeckCards;
+        
 
         public Deck()
         {
 
         }
 
-        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
-        public List<SpecialCard> SpecialCards1 { get => SpecialCards; set => SpecialCards = value; }
+        public List<Card> DeckCards { get => deckCards; set => deckCards = value; }
+        
 
-        public void AddCombatCard(CombatCard combatCard) { throw new NotImplementedException(); }
-        public void AddSpecialCard(SpecialCard specialCard) { throw new NotImplementedException(); }
-        public void DestroyCombatCard(int cardId) { throw new NotImplementedException(); }
-        public void DestroySpecialCard(int cardId) { throw new NotImplementedException(); }
+        public void AddDeckCard(Card deckCards) { throw new NotImplementedException(); }
+      
+        public void DestroyDeckCard(int cardId) { throw new NotImplementedException(); }
+        
         public void Shuffle() { 
             throw new NotImplementedException();
         }
